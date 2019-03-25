0e22a0c651e99f84a3f93270af5f9cf38e0ee831
Pakpoom Rachtrachoo <33973448+G30RG3PAK@users.noreply.github.com>
2019-03-25 12:28:36 +0700

    Simple variable and display in Java

---
+++ Myvar.java
@@ -0,0 +1,31 @@
+//Simple Java Variables
+//Syntax --> type variable = value;
+
+public class Myvar{
+	public static void main(String[] args){
+		String myname = "George";
+		System.out.println(myname);
+		
+		int mynum = 44;
+		System.out.println(mynum);
+		//another method to daclare a variable
+		int mynum2;
+		mynum2 = 55;
+		System.out.println(mynum2);
+		//declare variable of other types
+		float myfloat = 3.99f; //declare float
+		char myletter = 'A'; //declare character
+		boolean mylean = true;//declare boolean
+		String mytext = "Good Bye";//declare String
+		System.out.println(myfloat);
+		System.out.println(myletter);
+		System.out.println(mylean);
+		System.out.println(mytext);
+
+
+
+		
+
+
+	}
+}
