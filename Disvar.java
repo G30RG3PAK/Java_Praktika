72b30a19aff39c2dbfac7e23907a69fd77012f8b
Pakpoom Rachtrachoo <33973448+G30RG3PAK@users.noreply.github.com>
2019-03-26 14:42:50 +0700

    Display variable  in Java

---
+++ Disvar.java
@@ -0,0 +1,27 @@
+//Simple Java Variables
+//Syntax --> type variable = value;
+
+public class Disvar{
+	public static void main(String[] args){
+
+		String myname = "George";
+		System.out.println("Hello, My name is" + myname);
+
+		String firstname = "Pakpoom";
+		String lastname = "Rachtrachoo";
+		String fullname = firstname + lastname;
+		System.out.println(fullname);
+
+		int x;
+		int y;
+		x = 55;
+		y = 45;
+		System.out.println(x+y);
+
+		int a = 8, b = 10, c = 22, i = 20;
+		System.out.println(i+a+b+c);
+
+		
+
+	}
+}
