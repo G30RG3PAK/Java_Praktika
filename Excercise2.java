974b8d3f5c1b43db50d0d1632df913f32074d677
Pakpoom Rachtrachoo <33973448+G30RG3PAK@users.noreply.github.com>
2019-04-05 19:14:25 +0700

    Multiply 2 numbers in Java

--- Excercise2.java
+++ Excercise2.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Excercise3{
  public static void main(String[] args) {
- 	Scanner input = new Scanner(System.in);
+ 	
+	Scanner input = new Scanner(System.in);
  	System.out.print("Input the number: ");
  	int num1 = input.nextInt();
 
@@ -12,6 +13,6 @@ public class Excercise3{
  	int result = num1*num2;
  	//System.out.println();
  	System.out.println(num1+" x "+num2+ " = "+result);
-
+	 
 	}
 }
